4554a1d7349c55c934b14972c8b21964c03cab27
Vignesh Vijayakumar <vignesh.lions@gmail.com>
2022-01-24 23:27:09 +0100

    Prepare to host in github pages

--- docs/conf.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-#
-# -- General configuration -----------------------------------------------------
-
-source_suffix = '.rst'
-master_doc = 'index'
-project = u'VCA'
-copyright = u'2022 ATHENA Christian Doppler (CD) Laboratory'
-html_theme = "default"
